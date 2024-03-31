307956e9ffb6a572f470b07912a8e5bdafdb18be
vinodsagar260981 <vinodsagar0981@gmail.com>
2024-03-31 18:31:03 +0530

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+
+            <h1> 404 Not Found </h1>
+            <p>This Page doesn't exist </p>
+        </div>
+    )
+}
+
+export default NotFound
